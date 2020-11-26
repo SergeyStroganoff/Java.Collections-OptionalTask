1c28e8691ba2e017d3efe14dd350cc5ffee44852
Sergey Stroganov <111sergey90@gmail.com>
2020-11-26 13:52:28 +0300

    The first commit;

---
+++ src/com/stroganov/Task1.java
@@ -0,0 +1,64 @@
+package com.stroganov;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Task1 {
+
+
+    private String fileReadPath = "task.txt";
+    private String fileWritePath = "ksat.txt";
+
+    private List<String> stringList = new ArrayList<>();
+
+    public void setFileReadWritePath(String fileReadPath, String fileWritePath) {
+
+        this.fileReadPath = fileReadPath;
+        this.fileWritePath = fileWritePath;
+    }
+
+    public static boolean reverseFile(String filePathToRead, String filePathToWrite) {
+
+        boolean result = true;
+
+        ArrayList<String> stringList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePathToRead)))) { // StandardCharsets.UTF_8
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringList.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла : " + filePathToRead + e.getMessage());
+            result = false;
+        }
+
+        if (!stringList.isEmpty()) {
+
+            Collections.reverse(stringList);
+
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePathToWrite)))) { // StandardCharsets.UTF_8
+                String line;
+
+                for (String currentString : stringList) {
+                    writer.write(currentString);
+                    writer.append('\n');
+                }
+
+                writer.flush();
+
+            } catch (IOException e) {
+                System.out.println("Ошибка записи файла : " + filePathToRead + e.getMessage());
+                result = false;
+            }
+
+        }
+
+
+        return result;
+
+    }
+
+}
